efaa331271a3be44688ae38d8811f8943c783693
Diego Fernandez <152980541+colobus-delft@users.noreply.github.com>
2023-12-05 19:49:47 +0700

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="dashboard.php">Dashboard</a></li>
+    <li><a href="create_task.php">Create Task</a></li>
+    <li><a href="task_list.php">Task List</a></li>
+    <li><a href="logout.php">Logout</a></li>
+</ul>
