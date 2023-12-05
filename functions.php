1b69b8b8ba7bf39cc93f8d3f59f96dcf3bca00ee
Diego Fernandez <152980541+colobus-delft@users.noreply.github.com>
2023-12-05 19:49:36 +0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+function getTasks($user_id) {
+    // Fetch tasks from the database for a given user
+    // ...
+
+    return $tasks;
+}
+
+function createTask($user_id, $task_data) {
+    // Create a new task in the database
+    // ...
+
+    return $task_id;
+}
+
+// Other utility functions
